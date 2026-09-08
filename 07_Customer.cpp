231cc4406fafe2152b1e150e88037bcae80b4d74
saklanirohit33-rohit <saklanirohit33@gmail.com>
2026-09-08 09:15:56 +0530

    Create 07_Customer.cpp

---
+++ 07_Customer.cpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Customer {
+private:
+    string name;
+    string phone;
+
+public:
+    Customer(string name, string phone) {
+        this->name = name;
+        this->phone = phone;
+    }
+
+    string getName() {
+        return name;
+    }
+
+    string getPhone() {
+        return phone;
+    }
+};

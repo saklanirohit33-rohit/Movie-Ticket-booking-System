136c02ba60c3cddfa7a509dc1b1d79f65c0bc1ab
saklanirohit33-rohit <saklanirohit33@gmail.com>
2026-09-08 09:11:22 +0530

    Create 01_movie.cpp

---
+++ 01_movie.cpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Movie {
+private:
+    string title;
+    string language;
+    int duration;
+
+public:
+    Movie() {
+        title = "";
+        language = "";
+        duration = 0;
+    }
+
+    Movie(string title, string language, int duration) {
+        this->title = title;
+        this->language = language;
+        this->duration = duration;
+    }
+
+    string getTitle() {
+        return title;
+    }
+
+    string getLanguage() {
+        return language;
+    }
+
+    int getDuration() {
+        return duration;
+    }
+};
